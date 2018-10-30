398dab7c1049b3b90bfcc1880b8ea299cdbd2930
Tea <tdinhla@protonmail.com>
2018-10-29 17:02:39 -0700

    add user model and users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_165714) do
+ActiveRecord::Schema.define(version: 2018_10_30_000056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2018_10_28_165714) do
     t.string "publisher"
     t.string "genre"
     t.string "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
